967e66ebfdf4a677941d8d137c9fbefe9f61f0ef
Satyam2603 <CU25310014@coeruniversity.ac.in>
2025-11-10 12:55:36 +0530

    First occurrence of a character

---
+++ 12.cpp
@@ -0,0 +1,21 @@
+include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[100], ch;
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    cout << "Enter character to search: ";
+    cin >> ch;
+
+    char* pos = strchr(str, ch);
+
+    if (pos)
+        cout << "First occurrence at index: " << pos - str;
+    else
+        cout << "Character not found";
+
+    return 0;
+}
