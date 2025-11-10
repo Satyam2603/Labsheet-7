271d376c93b0338abb9c4a19d513d288c69c4e24
Satyam2603 <CU25310014@coeruniversity.ac.in>
2025-11-10 13:13:38 +0530

    Check if two strings are anagrams

---
+++ 20.cpp
@@ -0,0 +1,28 @@
+include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    char s1[100], s2[100];
+    cout << "Enter first string: ";
+    cin.getline(s1, 100);
+    cout << "Enter second string: ";
+    cin.getline(s2, 100);
+
+    if (strlen(s1) != strlen(s2)) {
+        cout << "Not anagrams";
+        return 0;
+    }
+
+    // Sort both strings
+    sort(s1, s1 + strlen(s1));
+    sort(s2, s2 + strlen(s2));
+
+    if (strcmp(s1, s2) == 0)
+        cout << "Strings are Anagrams";
+    else
+        cout << "Not Anagrams";
+
+    return 0;
+}
